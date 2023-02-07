0b7f3eac34af0a9fa0aa8f11c9ba58feb4e813d7
Ilia Reingold <ilia.reingold@gmail.com>
2023-02-07 14:10:05 -0500

    tweak

--- main.js
+++ main.js
@@ -9,4 +9,10 @@ console.log('el', el)
 
 el.addEventListener('save-and-export', (e) => {
   console.log('save-and-export', e, e.detail)
+  window.alert('save-and-export')
 }, false)
+
+
+// el.addEventListener('submit-target-panel', (e) => {
+//   console.log('submit', e, e.detail)
+// }, false)
