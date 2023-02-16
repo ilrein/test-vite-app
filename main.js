36871c8c0778ffe5da311177d92037a32707c085
Ilia Reingold <ilia.reingold@gmail.com>
2023-02-16 10:48:07 -0500

    udpate

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-import { register } from 'flighthub-vue'
-import 'flighthub-vue/dist/style.css'
-
+import { register } from 'airplane-ui'
 register()
-// console.log('test', register)
 
 const el = document.getElementById('precog-sales-display')
-console.log('el', el) 
-
-el.addEventListener('save-and-export', (e) => {
-  console.log('save-and-export', e, e.detail)
-  window.alert('save-and-export')
-}, false)
-
-
-// el.addEventListener('submit-target-panel', (e) => {
-//   console.log('submit', e, e.detail)
-// }, false)
